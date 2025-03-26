63c45b0a8d11c94f79dceeefc78ed35010b2e709
Tnhaan20 <nhancd909@gmail.com>
2025-03-27 01:15:24 +0700

    style: UI for news articles

--- NhanVT_MVC/obj/Debug/net8.0/NhanVT_MVC.AssemblyInfo.cs
+++ NhanVT_MVC/obj/Debug/net8.0/NhanVT_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NhanVT_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafdb7bd2f8f7ae745d44b06afbcba1959c69e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c51f46d82aa795ba96334d17183fbe1d49504f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NhanVT_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NhanVT_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
